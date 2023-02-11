9a24130526c3a4a50bfb6327a9a6b084ba40a647
Harin Wijesekara <30443272+harinwijesekara@users.noreply.github.com>
2023-02-11 19:32:48 +0530

    Version 14 develop reservation (#8)
    * Basic reservation functionality
    
    * Add Restaurant and Reservation to navigator
    
    * Change phone number validation on Restaurant and Reservation
    
    * Change phone number validation on Restaurant and Reservation
    
    ---------
    
    Co-authored-by: xeedling <122741158+xeedling@users.noreply.github.com>

--- erpnext/outlet/doctype/reservation/reservation.js
+++ erpnext/outlet/doctype/reservation/reservation.js
@@ -1,26 +1,17 @@
 // Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Reservation", {
+frappe.ui.form.on("Reservation", {
 // 	refresh(frm) {
 
 // 	},
-// });
-
-frappe.ui.form.on("Reservation List", {
-// 	refresh(frm) {
-
-// 	},
-
+    
     validate: function(frm) {
-        if(! /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/.test(frm.doc.phone)) {
-            msgprint('Invalid Phone Number, Add 10 digits number');
-            validated = false;
-        }
-    },
-
-    // table: function(frm) {
-    //   set_field_options("no_of_seats", frm.table.no_of_seats)
-    // },
-        
+        frm.doc.reservation_list.forEach(element => {
+            if(! /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/.test(element.phone)) {
+                msgprint('Invalid Phone Number, Add 10 digits number');
+                validated = false;
+            }
+        });
+    }
 });
